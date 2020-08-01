dd4081ad54bf41abf0210b0a8e92b44a81c32a36
shouvick <shouvick_shouvick@yahoo.com>
2020-08-02 01:52:35 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
+import {  Item, Input, Label, Button, List, ListItem } from 'native-base';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+if(!firebase.apps.length)
+{
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+export default class App extends React.Component{
+  state={
+    text:"",
+    mylist:[]
+  }
+  componentDidMount(){
+    const myitems = firebase.database().ref("mywishes")
+    myitems.on("value",datasnap=>{
+      if(datasnap.val()){
+      this.setState({mylist:Object.values(datasnap.val())})
+      }
+    })
+  }
+  saveitem(){
+    const mywishes = firebase.database().ref("mywishes")
+    mywishes.push().set({
+      text:this.state.text,
+      time:Date.now()
+    })
+    this.setState({text:""})
+  }
+  removeAll()
+  {
+    firebase.database().ref("mywishes").remove()
+    this.setState({mylist:[]})
+  }
+  
+  render(){
+    const myitems = this.state.mylist.map(item=>{
+      return(
+        <ListItem style={{justifyContent:"space-between"}}>
+        <Text>{item.text}</Text>
+        </ListItem>
+      )
+    })
+    return (
+      <View style={styles.container}>
+         <Item floatingLabel>
+                <Label>Add Items</Label>
+                <Input value={this.state.text} onChangeText={(text)=>this.setState({text})} />
+         </Item>
+         <View style={{flexDirection:"row",padding:20,justifyContent:"space-around"}}>
+            <Button rounded success
+            style={styles.mybtn}
+            onPress={()=>this.saveitem()}
+            >
+              <Text>ADD</Text>
+            </Button>
+            <Button rounded danger style={styles.mybtn} 
+            onPress={()=>this.removeAll()}
+            >
+              <Text>Delete All</Text>
+            </Button>
+         </View>
+         <List>
+          {myitems}
+         </List>
+      </View>
+      
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
   },
+  mybtn:{
+    padding:10,
+    width:160,
+    justifyContent:"center"
+  },
+  text: {
+    color: "white",
+    fontSize: 25
+  }
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+# React Native Todolist
+
+# Features!
+  - Add item
+  - Fetch item
+  - Delete item
+
+### Tech
+
+* React Native
+* NativeBase
+* Firebase 
+
+### Firebase Configuaration 
+You can find firebase configuration on config.js.Please set these values:
+  - apiKey: "",
+  - authDomain: "",
+  - databaseURL: "",
+  - projectId: "",
+  - storageBucket: "",
+  - messagingSenderId: "",
+  - appId: ""
+
+
+
+
+
